35009b04b85022708296fbc85acd284b449f9695
orokgit <lorl@mail.ru>
2014-12-03 13:38:46 +0300

    config

--- app/src/main/java/com/javapapers/android/chat/GcmRegistrationAsyncTask.java
+++ app/src/main/java/com/javapapers/android/chat/GcmRegistrationAsyncTask.java
@@ -4,13 +4,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
-import com.example.mymodule.backend.registration.Registration;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClient;
 
 import java.io.IOException;
 import java.util.logging.Level;
@@ -20,7 +14,6 @@ import java.util.logging.Logger;
  * Created by orok on 25.11.2014.
  */
 class GcmRegistrationAsyncTask extends AsyncTask<Context, Void, String> {
-    private static Registration regService = null;
     private GoogleCloudMessaging gcm;
     private Context context;
 
@@ -29,26 +22,6 @@ class GcmRegistrationAsyncTask extends AsyncTask<Context, Void, String> {
 
     @Override
     protected String doInBackground(Context... params) {
-        if (regService == null) {
-//            Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(),
-//                    new AndroidJsonFactory(), null)
-//                    // Need setRootUrl and setGoogleClientRequestInitializer only for local testing,
-//                    // otherwise they can be skipped
-//                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-//                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-//                        @Override
-//                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest)
-//                                throws IOException {
-//                            abstractGoogleClientRequest.setDisableGZipContent(true);
-//                        }
-//                    });
-//            // end of optional local run code
-
-            Registration.Builder builder = new Registration.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null);
-
-            regService = builder.build();
-        }
-
         context = params[0];
 
         String msg = "";
